74dd5c3f5295320e0f5cbfbf1a0daa56db617b3d
vbetsch <vic.betsch@gmail.com>
2024-07-25 00:22:56 +0200

    :zap: _app.jsx -> _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
+import {AppProps} from "next/app";
 
-export default function App({Component, pageProps}) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <>
             <Head>
