fede5089ea2d2ed765e10f607965f0d4aebd65e7
vbetsch <vic.betsch@gmail.com>
2024-07-25 00:12:13 +0200

    :fire: Clean template

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {Html, Head, Main, NextScript} from 'next/document';
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Notion Like</title>
+            </Head>
+            <body style={{margin: 0}}>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
+}
